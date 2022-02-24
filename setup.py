6ec65f76cfa98bd06cc2a354568e6d615d3cbfc3
Rafael Pastrana <arpj@princeton.edu>
2022-02-24 10:28:22 -0500

    Added setup.py to allow editable install in conda

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='nfem',
+      version='4.0.0-dev',
+      description='NFEM Teaching Tools',
+      url='https://github.com/StatikTUM/nfem',
+      packages=['nfem', 'nfem.visualization'],
+      include_package_data=True,
+      zip_safe=False)
